fcf11081e455a26d6b705cb84975a8588b0f15f8
mujadadrao <mujadadrao@gmail.com>
2020-10-08 01:51:41 +0500

    basic auth added

--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import 'semantic-ui-css/semantic.min.css';
+import reducers from "./reducers";
+import {createStore, compose} from "redux";
+import {middlewares, sagaMiddleware} from './store/middlewares';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(middlewares));
+
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +19,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
